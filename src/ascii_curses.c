540545305da381773f0da77b32a65eb425e5476e
Edgar Mendoza <edorizz_99@hotmail.com>
2017-02-18 15:10:00 -0800

    Improved black/white printing to grayscale

--- src/ascii_curses.c
+++ src/ascii_curses.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+
 #include "../include/ascii_curses.h"
 
 #define BLOCK_SIZE	8
@@ -8,20 +10,28 @@ void usage(void)
 	printf("usage: ascii_curses <file_path.png>\n");
 }
 
+BYTE grayscale_value(pixel_t *pixel)
+{
+	return (pixel->r + pixel->g + pixel->b) / 3;
+}
+
 void print_ascii(bitmap_t *bmp)
 {
+	const char *greyscale_chars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
 	pixel_t *pixel;
-	int i, j;
+	int i, j, c;
+	float scale;
 
+	scale = 255.0f / (float)strlen(greyscale_chars);
 	for (i = 0; i < bmp->height; i += BLOCK_SIZE) {
 		for (j = 0; j < bmp->width; j += BLOCK_SIZE) {
 			pixel = &bmp->pixel_data[i * bmp->width + j];
+			c = greyscale_chars[(int)(grayscale_value(pixel) / scale)];
+
 			if (pixel->a == 0)
 				printf("  ");
-			else if (pixel->r == 0 && pixel->g == 0 && pixel->b == 0)
-				printf("++");
 			else
-				printf("##");
+				printf("%c%c", c, c);
 		}
 		putchar('\n');
 	}
@@ -37,7 +47,11 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	bmp = read_png_image(argv[1]);
+	if ((bmp = read_png_image(argv[1])) == NULL) {
+		fprintf("%s: Failed to read %d\n", argv[0], argv[1]);
+		return 1;
+	}
+
 	print_ascii(bmp);
 
 	return 0;
